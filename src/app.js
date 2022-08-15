29f171d8f092d28fc48a3cacd505c08186403865
Jorge Oviedo <jorgeoviedo1999@gmail.com>
2022-08-15 02:31:50 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,29 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+let excuses = document.getElementById("excuses");
+
+const random = arr => {
+  return Math.floor(Math.random() * arr.length);
+};
+
+const concatArr = () => {
+  return who[random(who)]
+    .concat(" ")
+    .concat(what[random(what)])
+    .concat(" ")
+    .concat(when[random(when)]);
+};
+
+excuses.innerHTML = concatArr();
